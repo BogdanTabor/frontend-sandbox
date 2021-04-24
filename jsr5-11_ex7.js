e1a64ab1c93a22b7a82e995e66c01d231bb262df
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-04-24 11:21:19 +0300

    Update jsr5-11_ex7.js

--- jsr5-11_ex7.js
+++ jsr5-11_ex7.js
@@ -1,4 +1,4 @@
-function getSecondsToTOmorrow() {
+function getSecondsToTomorrow() {
 	let now = new Date();
 	let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
 	return Math.round((tomorrow - now) / 1000);
