f413ad33535a42b33a47ccd5d7415a6f0708cbf9
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-04-26 22:09:33 +0300

    add the solution to ex. 1 from JSR chapter 6.8

--- jsr6-8_ex1.js
+++ jsr6-8_ex1.js
@@ -11,5 +11,17 @@ function printNumbers(from, to) {//recursive setTimeout
 }
 
 
+function printNumbers(from, to) {//setInterval
+  let current = from;
+
+  let timerId = setInterval(function() {
+    alert(current);
+    if(current==to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
+}
+
 
 printNumbers(1, 5);
