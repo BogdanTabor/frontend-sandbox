35e09997ef4c0a0d8237e3a5cbee1030d66982fe
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-05-24 23:04:02 +0300

    add the practicing from JSR chapter 7.1

---
+++ jsr7-1.js
@@ -0,0 +1,31 @@
+let user = {
+  name: "John"
+};
+
+let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+
+alert( JSON.stringify(descriptor, null, 2 ) );
+/* property descriptor:
+{
+  "value": "John",
+  "writable": true,
+  "enumerable": true,
+  "configurable": true
+}
+*/
+
+Object.defineProperty(user, "name", {
+  value: "John"
+});
+
+let descriptor = Object.getOwnPropertyDescriptor(user, 'name');
+
+alert( JSON.stringify(descriptor, null, 2 ) );
+/*
+{
+  "value": "John",
+  "writable": false,
+  "enumerable": false,
+  "configurable": false
+}
+ */
