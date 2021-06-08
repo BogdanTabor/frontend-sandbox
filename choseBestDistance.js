2504c759a4dcd97645a427ce07c0b5f4471c387a
BogdanTabor <bogdan.tabor@gmail.com>
2021-06-08 13:44:10 +0300

    add radency test task choose best distance

---
+++ choseBestDistance.js
@@ -0,0 +1,7 @@
+const chooseBestDistance = (t, k, ls) => {
+    //code goes here
+    return null;
+}
+
+chooseBestDistance(174, 3, [51, 56, 58, 59, 61]); //173
+chooseBestDistance(163, 3, [50]); // null
