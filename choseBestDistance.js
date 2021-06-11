bd393cef9be33adf535ce3359ec5df59e1e2bdfa
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-06-11 22:34:49 +0300

    done task with 3 levels

--- choseBestDistance.js
+++ choseBestDistance.js
@@ -1,35 +1,37 @@
-const chooseBestDistance = (t, k, ls) => {
+// const chooseBestDistance = (t, k, ls) => {
 
-  //here  will be recursion
-	let arr = ls;
-	if (arr.length == k) {
-		return arr 
-	} else {
-		return arr.slice(0, k)
-	}
+//   //here  will be recursion
+// 	let arr = ls;
+// 	if (arr.length == k) {
+// 		return arr 
+// 	} else {
+// 		return arr.slice(0, k)
+// 	}
 	
-	// } else {
-	// 	return 
-	// }
-	// for (let i = 0; i < ls.length; ++i) {
-	// 	arr.push(ls[i])
-	// 	}
-}
-
-chooseBestDistance(174, 3, [51, 56, 58, 59, 61]); //173
-// chooseBestDistance(163, 3, [50]); // null
-
-// const bestDistanceFromMaxic = (t, k, ls) => {
-//     listOfSums = [];
-//     for (i = 0; i < ls.length; i++){
-//     	for (z = i+1; z < ls.length; z++){
-//       	for (f = z+1; f < ls.length; f++){
-//         	listOfSums.push([ls[i], ls[z], ls[f]]);
-//       }
-//     }
-//   }
-// 	return listOfSums
+// 	// } else {
+// 	// 	return 
+// 	// }
+// 	// for (let i = 0; i < ls.length; ++i) {
+// 	// 	arr.push(ls[i])
+// 	// 	}
 // }
 
-// bestDistanceFromMaxic(174, 3, [51, 56, 58, 59, 61])
-//ty tut?
+const chooseBestDistance = (t, k, ls) => {
+	
+	bestSum = 0;
+	for (i = 0; i < ls.length; i++) {
+		for (j = i + 1; j < ls.length; j++) {
+			for (k = j + 1; k < ls.length; k++) {
+				temp = ls[i] + ls[j] + ls[k]
+				if ( temp >= bestSum && temp <= t) {
+					bestSum = temp;
+				}
+			}
+		}
+	}
+	return bestSum;
+	return null
+}
+
+console.log(chooseBestDistance(174, 3, [51, 56, 58, 59, 61])); //173
+console.log(chooseBestDistance(163, 3, [50])); // null
