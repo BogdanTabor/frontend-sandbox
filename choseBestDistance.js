ce23d6495ab11e8ed98cfa86eb843a330d975e19
Bogdan Tabor <bogdan.tabor@gmail.com>
2021-06-13 17:09:04 +0300

    done working function but with bug

--- choseBestDistance.js
+++ choseBestDistance.js
@@ -1,47 +1,50 @@
-// const chooseBestDistance = (t, k, ls) => {
+// example of code
+// http://espressocode.top/print-all-possible-combinations-of-r-elements-in-a-given-array-of-size-n/
+// https://question-it.com/questions/1476389/najti-vse-vozmozhnye-kombinatsii-podmnozhestv-v-massive
+
+
 // // t (максимальна сума відстаней, int >= 0)
 // // k (кількість міст, які потрібно відвідати, k >= 1)
 // // ls (список відстаней int >= 0 && містить принаймні один елемент)
 // // return bestSum, найбільшу суму k відстаней, <= t (якщо існує) : null
 
-// 	let sum = 0;
-// 	if (ls.length >= 1 && t >= 0 && k >= 1) {
-// 		//make subArr
-		
-// 			for (let elem of ls) {
-// 					let tempSum = 0
-// 					tempSum += elem
-// 					if (tempSum > sum) {
-// 						sum = tempSum
-// 					}
-// 			}
-// 		return sum
-// 	} else {
-// 	return null
-// 	}
-// }
+const chooseBestDistance = (t, k, ls) => {
+	let sum = 0, tempSum = 0;
 
-// chooseBestDistance(174, 3, [51, 56, 58, 59, 61]); //173
+	if (ls.length >= 1 && t >= 0 && k >= 1) {
+		function* generateCombinations(arr, size) {
+			function* doGenerateCombinations(offset, combo) {
+				if (combo.length == size) {
+					yield combo;
+				} else {
+					for (let i = offset; i < arr.length; i++) {
+						yield* doGenerateCombinations(i + 1, combo.concat(arr[i]));
+					}
+				}
+			}
+			yield* doGenerateCombinations(0, []);
+		}
 
-function* generateCombinations(arr, size) {
-  function* doGenerateCombinations(offset, combo) {
-    if (combo.length == size) {
-      yield combo;
-    } else {
-      for (let i = offset; i < arr.length; i++) {
-        yield* doGenerateCombinations(i + 1, combo.concat(arr[i]));
-      }
-    }
-  }
-  yield* doGenerateCombinations(0, []);
+	for (let combo of generateCombinations(ls, k)) {
+		for (i = 0; i < combo.length; i++) {
+			tempSum += combo[i]
+			if (tempSum > sum && tempSum < t) {
+				sum = tempSum;
+				tempSum = 0;
+			}
+		}
+	}
+	return sum
+	} else {
+	return null
+	}
 }
 
-for (let combo of generateCombinations([1, 2, 3, 4, 5], 2)) {
-  console.log(JSON.stringify(combo));
-}
+chooseBestDistance(174, 3, [51, 56, 58, 59, 61]); //173
+// chooseBestDistance(163, 3, []); // null
 
 
-// const chooseBestDistance = (t, k, ls) => {
+// const chooseBestDistance = (t, k, ls) => { //chooseBestDistance for 3 iteration
 // 	sum = 0;
 // 	for (i = 0; i < ls.length; i++) {
 // 		for (j = i + 1; j < ls.length; j++) {
